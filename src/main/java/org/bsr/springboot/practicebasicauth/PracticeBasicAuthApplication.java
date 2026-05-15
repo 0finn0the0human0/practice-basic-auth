b9383742978df2c2172b188cf64012a1e7192615
finn_the_human <sbenjamin726@outlook.com>
2026-05-14 23:41:43 -0500

    chore: build and dependency setup

---
+++ src/main/java/org/bsr/springboot/practicebasicauth/PracticeBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package org.bsr.springboot.practicebasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBasicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBasicAuthApplication.class, args);
+    }
+
+}
